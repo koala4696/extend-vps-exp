423522b6f49611c3c788e2aa8a22e4b5575b3290
Mia4696 <koala21.mia@outlook.jp>
2025-10-20 14:38:48 +0900

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -38,6 +38,7 @@ try {
     const body = await page.$eval('img[src^="data:"]', img => img.src)
     const code = await fetch('https://captcha-120546510085.asia-northeast1.run.app', { method: 'POST', body }).then(r => r.text())
     await page.locator('[placeholder="上の画像の数字を入力"]').fill(code)
+    await page.locator('text=私がボットではないことを確認します。').click()
     await page.locator('text=無料VPSの利用を継続する').click()
 } catch (e) {
     console.error(e)
